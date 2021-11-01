2fa5eb37d140eb6f42583ecabba619cecb7f12d0
yangxin <yx5316@dingtalk.com>
2021-11-01 21:22:23 +0800

    回滚

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,7 @@
-// import NextImage from 'next/image'
-// eslint-disable-next-line jsx-a11y/alt-text
-// const Image = ({ ...rest }) => <NextImage {...rest} />
-// https://github.com/timlrx/tailwind-nextjs-starter-blog/issues/231
-const Image = ({ ...rest }) => <img  {...rest} />
+import NextImage from 'next/image'
 
+// eslint-disable-next-line jsx-a11y/alt-text
+const Image = ({ ...rest }) => <NextImage {...rest} />
+// https://github.com/timlrx/tailwind-nextjs-starter-blog/issues/231
+// const Image = ({ ...rest }) => <img  {...rest} />
 export default Image
