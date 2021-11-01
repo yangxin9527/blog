8989a3804165d9434f98bbdcd98ad225c42b99ac
yangxin <yx5316@dingtalk.com>
2021-11-01 19:48:42 +0800

    img 修改

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,7 @@
-import NextImage from 'next/image'
-
+// import NextImage from 'next/image'
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />
+// const Image = ({ ...rest }) => <NextImage {...rest} />
+// https://github.com/timlrx/tailwind-nextjs-starter-blog/issues/231
+const Image = ({ ...rest }) => <img  {...rest} />
 
 export default Image
